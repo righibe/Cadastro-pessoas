1b0195526f6efa5be5b7f17bbfb47a43c9320592
righibe <bernardomicolrighi@outlook.com>
2025-10-22 19:03:28 -0300

    atualizando pastas

--- src/main/java/dev/righi/CadastroDePessoas/CadastroDePessoasApplication.java
+++ src/main/java/dev/righi/CadastroDePessoas/CadastroDePessoasApplication.java
@@ -8,6 +8,11 @@ public class CadastroDePessoasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroDePessoasApplication.class, args);
+
+        System.out.println("hello world");
+
+
+
 	}
 
 }
