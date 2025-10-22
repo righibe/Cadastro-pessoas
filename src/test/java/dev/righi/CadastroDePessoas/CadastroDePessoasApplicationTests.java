74bb256fbd97b5868092449145fcda47e45b4ba6
righibe <bernardomicolrighi@outlook.com>
2025-10-22 18:57:03 -0300

    verificando repositorio

---
+++ src/test/java/dev/righi/CadastroDePessoas/CadastroDePessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.righi.CadastroDePessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDePessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
