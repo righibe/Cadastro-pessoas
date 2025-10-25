45def2fe88e6fa37f5e17029fb964cabb0c2aa3d
righibe <bernardomicolrighi@outlook.com>
2025-10-24 23:47:26 -0300

    fiz o elemento pessoa

---
+++ src/main/java/dev/righi/CadastroDePessoas/PessoaModel.java
@@ -0,0 +1,42 @@
+package dev.righi.CadastroDePessoas;
+
+public class PessoasModel {
+
+
+    String nome;
+    String email;
+    int idade;
+
+    public PessoasModel() {
+    }
+
+    public PessoasModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
